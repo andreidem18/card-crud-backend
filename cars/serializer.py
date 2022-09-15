8d75b90b9d6cc1c8959d808455b760a5ff962a75
Andres Mendoza <andres.david.mm@hotmail.com>
2022-09-15 16:18:38 -0500

    added serializer functionality

--- cars/serializer.py
+++ cars/serializer.py
@@ -5,3 +5,8 @@ class CarSerializer(ModelSerializer):
     class Meta:
         model = Car
         fields = ('id', 'brand', 'model', 'color', 'year', 'price')
+
+class CreateCarSerializer(ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ('id', 'brand', 'model', 'color', 'year', 'price', 'created_by')
--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from cars.models import Car
-from cars.serializer import CarSerializer
+from cars.serializer import CarSerializer, CreateCarSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from config.get_client_ip import get_client_ip
@@ -16,13 +16,11 @@ class CarViewSet(ModelViewSet):
         return Response(serialized.data)
 
     def create(self, request, *args, **kwargs):
-        car = Car.objects.create(
-            model=request.data['model'], 
-            brand=request.data['brand'], 
-            color=request.data['color'], 
-            year=request.data['year'], 
-            price=request.data['price'],
-            created_by=get_client_ip(request)
-        )
-        serialized = CarSerializer(car)
-        return Response(status = status.HTTP_201_CREATED, data = serialized.data)
+        request.data['created_by']=get_client_ip(request)
+        serialized = CreateCarSerializer(data=request.data)
+        if not serialized.is_valid():
+            return Response(status = status.HTTP_400_BAD_REQUEST, data = serialized.errors)
+        else:
+            serialized.save()
+            serialized = CarSerializer(serialized.data)
+            return Response(status = status.HTTP_201_CREATED, data = serialized.data)
